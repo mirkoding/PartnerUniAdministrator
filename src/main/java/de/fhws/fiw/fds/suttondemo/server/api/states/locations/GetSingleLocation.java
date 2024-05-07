3c9150051abaa57ef124f9a5d1fa92a4a38a6652
Peter Braun <p.braun@computer.org>
2024-05-07 09:43:09 +0200

    Initial

---
+++ src/main/java/de/fhws/fiw/fds/suttondemo/server/api/states/locations/GetSingleLocation.java
@@ -0,0 +1,58 @@
+package de.fhws.fiw.fds.suttondemo.server.api.states.locations;
+
+import de.fhws.fiw.fds.sutton.server.api.caching.CachingUtils;
+import de.fhws.fiw.fds.sutton.server.api.caching.EtagGenerator;
+import de.fhws.fiw.fds.sutton.server.api.states.AbstractState;
+import de.fhws.fiw.fds.sutton.server.api.states.get.AbstractGetState;
+import de.fhws.fiw.fds.sutton.server.database.results.SingleModelResult;
+import de.fhws.fiw.fds.sutton.server.models.AbstractModel;
+import de.fhws.fiw.fds.suttondemo.server.DaoFactory;
+import de.fhws.fiw.fds.suttondemo.server.api.models.Location;
+import de.fhws.fiw.fds.suttondemo.server.api.states.persons.PersonUri;
+
+public class GetSingleLocation<R> extends AbstractGetState<R, Location> {
+
+    public GetSingleLocation( final Builder<R> builder )
+    {
+        super( builder );
+    }
+
+    @Override
+    protected void authorizeRequest() {
+
+    }
+
+    @Override protected SingleModelResult<Location> loadModel( )
+    {
+        return DaoFactory.getInstance( ).getLocationDao( ).readById( this.requestedId );
+    }
+
+    @Override
+    protected boolean clientKnowsCurrentModelState(AbstractModel modelFromDatabase) {
+        return this.suttonRequest.clientKnowsCurrentModel(modelFromDatabase);
+    }
+
+    @Override
+    protected void defineHttpCaching() {
+        final String eTagOfModel = EtagGenerator.createEtag(this.requestedModel.getResult());
+        this.suttonResponse.entityTag(eTagOfModel);
+        this.suttonResponse.cacheControl(CachingUtils.create30SecondsPublicCaching());
+    }
+
+    @Override protected void defineTransitionLinks( )
+    {
+        addLink( LocationUri.REL_PATH_ID, LocationRelTypes.UPDATE_SINGLE_LOCATION, getAcceptRequestHeader( ),
+                this.requestedId );
+        addLink( PersonUri.REL_PATH_ID, LocationRelTypes.DELETE_SINGLE_LOCATION, getAcceptRequestHeader( ),
+                this.requestedId );
+    }
+
+    public static class Builder<R> extends AbstractGetStateBuilder<R, Location>
+    {
+        @Override public AbstractState<R, Location> build( )
+        {
+            return new GetSingleLocation<>( this );
+        }
+    }
+
+}
